d3f2ac1885b2040ae2626255ba9ee17bffe1c06f
prajy <prajwalvepicx@gmail.com>
2017-05-22 11:55:25 -0400

    Add files via upload

--- FrameworkApp/src/application/AppController.java
+++ FrameworkApp/src/application/AppController.java
@@ -3,7 +3,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -21,7 +24,6 @@ public class AppController {
 	
 	@FXML
 	private CheckBox gpuAccelBox;
-	
 	@FXML
 	private CheckBox hsvBox;
 	@FXML
@@ -32,18 +34,20 @@ public class AppController {
 	private CheckBox zmqBox;
 	@FXML
 	private Button submitButton;
-	
-
+	@FXML
+	private ChoiceBox<String> cameraDropdown;
+    @FXML
+    private CheckBox exposureBox;
 	@FXML
 	private void initialize()  {
-		gpuAccelBox.setSelected(true);
+		cameraDropdown.setItems(FXCollections.observableArrayList("1", "2"));
 	}
 
 	 @FXML
 	 private void handleButtonAction(ActionEvent event) throws IOException {
-	     // Button was clicked, do something...
-	     System.out.println("Button CLicked!");
-	     BufferedWriter writer = new BufferedWriter(new FileWriter("test.cpp"));
+	     System.out.println("Generating File...");
+	     BufferedWriter writer = new BufferedWriter(new FileWriter("file.cpp"));
+	     
 	     writer.write("#include <opencv2/highgui/highgui.hpp> \n"
 					+ "#include <opencv2/imgproc/imgproc.hpp> \n"
 					+ "#include <stdio.h> \n"
@@ -61,9 +65,47 @@ public class AppController {
 	    	 writer.write("#include " + "\"opencv2/gpu/gpu.hpp\" \n" );
 	     }
 	     
+	     writer.write("\nint main() {");
+	     
+	     if(zmqBox.isSelected()) {
+	    	 writer.write("\nzmq::context_t context (1);");
+	    	 writer.write("\nzmq::socket_t publisher(context, ZMQ_PUB);");
+	    	 // Replace 5801 with port variable from text area
+	    	 writer.write("\npublisher.bind" + "(\"tcp://*:" + "5801" + ");");
+	    	 
+	     }
+	     if(networkTableBox.isSelected()) {
+	    	 writer.write("\nNetworkTable::SetClientMode();");
+	    	 writer.write("\nNetworkTable::SetIPAddress(\"10.63.25.2\";");
+	    	 writer.write("\nNetworkTable::Initialize();");
+	    	 writer.write("\n std::shared_ptr<NetworkTable> table = NetworkTable::GetTable(\"vision\")");
+
+        //table->PutNumber("X", 5);");
+	    	}
+	     if(exposureBox.isSelected()) {
+	    	 // Can for loop this till the number of cameras if the arraylist contains integers.
+	    	 if(cameraDropdown.getValue() == "1"){
+	    		 // Replace video0 with indices that user inputs
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video0 -c exposure_auto=1\")");
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video0 -c exposure_absolute=5\")");
+	    	 }
+	    	 if(cameraDropdown.getValue() == "2"){
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video0 -c exposure_auto=1\")");
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video1 -c exposure_auto=1\")");
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video0 -c exposure_absolute=5\")");
+	    		 writer.write("\nsystem" + "(\"v4l2-ctl -d /dev/video1 -c exposure_absolute=5\")");
+	    	 } 
+	     }
+	    
+	     
+	     writer.flush();
 	     writer.close();
 	     
+	     
+	     
 	 }
+	 
+	 
 
 }
 
