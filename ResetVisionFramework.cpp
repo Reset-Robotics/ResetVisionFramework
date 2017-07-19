6622c8ea4407a552419b27d710873291469b1bf7
Prajwal Vedula <prajwalvepicx@gmail.com>
2017-07-19 08:43:04 -0400

    v1..1 Released!

--- ResetVisionFramework.cpp
+++ ResetVisionFramework.cpp
@@ -2,26 +2,26 @@
  Reset Robotics, FRC Team #6325 - Reset Vison Framework
  
  * Name          : Reset Vision Framework
- * Version       : 0.5
- * Build Date    : 12 June 2017
- * Last Update   : 11 July 2017
+ * Version       : 1.1
+ * Build Date    : 19 July 2017
+ * Last Update   : 19 July 2017
  * Developed By  : Reset Robotics, FRC Team #6325
  * Author(s)     : Reset Robotics - Prajwal Vedula
  
  Copyright (C) 2017 Reset Robotics
-
-/*
- This is the Reset Vision Framework. 
+ 
+ /*
+ This is the Reset Vision Framework.
  
  There are 7 steps to the Reset Framework code.
  1. Includes
  2. Initializing variables and communication
  3. Performing GPU Accelerated operations
  4. Performing regular operations
- 5. Performing any extra filtering 
+ 5. Performing any extra filtering
  6. Calculations
  7. Sending the Data
-
+ 
  */
 // Step One
 
@@ -64,7 +64,7 @@ int main() {
     printf("Current exposure settings:\n");
     system("v4l2-ctl -d /dev/video0 -C exposure_auto");
     system("v4l2-ctl -d /dev/video0 -C exposure_absolute");
-   
+    
     
     //Set exposure. Change the exposure_absolute to whatever exposure you want. Keep exposure_auto at 1
     system("v4l2-ctl -d /dev/video0 -c exposure_auto=1");
@@ -99,7 +99,7 @@ int main() {
     
     // GpuMats for GPU Acceleration. If you need any more GPUMats add them here
     cv::gpu::GpuMat src, resize, hsv, threshold;
- 
+    
     
     //HSV Threshold Sliders (can comment out for use on field)
     
@@ -119,33 +119,35 @@ int main() {
     cv::createTrackbar("Value Upper Bound", "HSV Thresholding", &v_upperb, 255);
     
     // Add another contours variable for another camera if necessary
+    // Contours is the variable in which the original contours that are found will be stored
+    // filteredContours will be where the filtered contours are stored.
     std::vector<std::vector<cv::Point> > contours, filteredContours; //array of contours (which are each an array of points)
-
+    
     
     //start the video. Change to whatever indices you are using.. Add another videoCapture for another camera.
     cv::VideoCapture leftCamera(0);
     cv::VideoWriter output;
     
-    // Variables used for the offset of center targets to camera. Can add another variable for more cameras.
+    // Variables used for the calculations. Center is the CenterX in this example. Can add another variable for more cameras/more things to calculate.
     
-    double offset = 0;
+    double center = 0;
     
     // Step Three
     //loop for each frame
     for (;;) {
-        //reads the image from each camera. Can add another if statement with videocapture name for more cameras.
+        //reads the image from each camera. Can add another if statement with videocapture and image name for more cameras.
         if (!leftCamera.read(imgRaw)) {
             break;
         }
-    
         
-        /* The GPU Acceleration Parts for a Jetson TX1. If you are using a Pi or something else delete the GPU Accel parts and perform all operaions in the next section which is the non accelerated functions. Only certain functions can be accelerated.
-       You can do any regular functions compatible functions for GPU Accel but you must put the input and output as GpuMats and do cv::gpu::function. First you must convert the image from camera to a GpuMat. Do gpuMatName.upload(regularMatName); Now you can perform any operations. For resize do cv::gpu::resize(gpuInputMat, gpuOutputMat, width, height). Then you can use the gpuOutputMat into a different operation such as cvtColor. To get the gpuMat back to a regularMat do gpuMatName.download(regularMatName); The code below resizes the image from camera and converts it into the HSV Colorspace. If using multiple cameras remember to perform your operations to both cameras.
+        
+        /* The GPU Acceleration Parts for a Jetson TK1/TX1/TX2. If you are using a Pi or something else delete the GPU Accel parts and perform all operaions in the next section which is the non accelerated functions. Only certain functions can be accelerated.
+         You can do any regular functions compatible functions for GPU Accel but you must put the input and output as GpuMats and do cv::gpu::function. First you must convert the image from camera to a GpuMat. Do gpuMatName.upload(regularMatName); Now you can perform any operations. For resize do cv::gpu::resize(gpuInputMat, gpuOutputMat, width, height, interpolation). Then you can use the gpuOutputMat into a different operation such as cvtColor. To get the gpuMat back to a regularMat do gpuMatName.download(regularMatName); The code below resizes the image from camera and converts it into the HSV Colorspace. If using multiple cameras remember to perform your operations to both cameras.
          */
-    
+        
         //upload regular Mat to GPU as a GpuMat
         src.upload(imgRaw);
-
+        
         
         //gpu accelerated image transformations
         cv::gpu::resize(src, resize, cv::Size(leftCamera.get(CV_CAP_PROP_FRAME_WIDTH) / img_scale_factor,
@@ -169,9 +171,9 @@ int main() {
         cv::inRange(imgHSV, cv::Scalar(h_lowerb, s_lowerb, v_lowerb), cv::Scalar(h_upperb, s_upperb, v_upperb), imgThreshold);
         
         imgThreshold.copyTo(imgContour);
-    
+        
         cv::findContours(imgContour, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    
+        
         // Step Five
         // Here you can do various methods and functions to filter out contours and what not.
         
@@ -179,30 +181,30 @@ int main() {
         // The contours that pass the tests will be pushed into the variable filteredContours.
         // You can add/remove any filteration tests within this for loop.
         filteredContours.clear();
-        for (std::vector<cv::Point> currentContour: contours) // enhanced for loop 
+        for (std::vector<cv::Point> currentContour: contours) // enhanced for loop
         {
-         Rect boundRect = boundingRect(contours[i]);
-         float contourArea = contourArea(contours[i]);  
-         float aspectRatio = (float)boundRect.width/boundRect.height // Contour width/ contour height
-         float solidityRatio = contourArea(contours[i])/(boundRect.width*boundRect.height); // the closer ratio to one, the more rectangle-y it is.
-         if (contourArea > maxArea || contourArea < minArea) continue;
-         if (aspectRatio > maxAspectRatio || aspectRatio < minAspectRatio) continue;
-         if (solidityRatio > maxSolidityRatio || solidityRatio < minASolidityRatio) continue;
-     
-         filteredContours.push_back(currentContour);
-
+            Rect boundRect = boundingRect(contours[i]);
+            float contourArea = contourArea(contours[i]);
+            float aspectRatio = (float)boundRect.width/boundRect.height // Contour width/ contour height
+            float solidityRatio = contourArea(contours[i])/(boundRect.width*boundRect.height); // the closer ratio to one, the more rectangle-y it is.
+            if (contourArea > maxArea || contourArea < minArea) continue;
+            if (aspectRatio > maxAspectRatio || aspectRatio < minAspectRatio) continue;
+            if (solidityRatio > maxSolidityRatio || solidityRatio < minASolidityRatio) continue;
+            
+            filteredContours.push_back(currentContour);
+            
         }
-       
+        
         
         // End various filtering
         
         // Step Six
         
-        /* This is where the calculation happen. filteredContours is the output from all the filtering. 
+        /* This is where the calculation happen. filteredContours is the output from all the filtering.
          
          */
         
-        //If the filtered contours are not empty (is there any contours that passed the filtering)
+        //If the filtered contours are not empty (is there any contours that passed the filtering), then calculate centerX.
         // You can change this if statement to only calculate under conditions you want/
         // For example, in Steamworks there were 2 vision tapes at the peg so you could do filteredContours==2 or filteredContours>=2
         if (!filteredContours.empty()) {
@@ -214,27 +216,28 @@ int main() {
             std::cout << "Offset" << center << std::endl;
             
         }
+        // This is optional. We use it so that the robot knows no target is in view. Use a number that your CenterX will not be (centerX can be zero depending on your case so be careful).
         else{
             center = 0; //if no target is detected, send 0
         }
         // Step 7
         // Sends message using your communication protocl. Delete following 3 lines if not using ZMQ.
-        zmq::message_t message(20);
-        snprintf ((char *) message.data(), 20, "displacement %f %f", center;
-        publisher.send(message);
-        
+            zmq::message_t message(20);
+            snprintf ((char *) message.data(), 20, "displacement %f %f", center;
+            publisher.send(message);
+                  
         // Send values via NetworkTables or other communication protocol here
-        
-        // shows thresholded image
-        cv::imshow("stream", imgThreshold);
-        
-        //if spacebar is pressed, quit the program
-        char c = cv::waitKey(1);
-        if (c == ' ') {
-            printf("Stream has ended.");
-            break;
-        }
-        
-        
-        
-    }
+                  
+                  // shows thresholded image
+                  cv::imshow("stream", imgThreshold);
+                  
+                  //if spacebar is pressed, quit the program
+                  char c = cv::waitKey(1);
+                  if (c == ' ') {
+                      printf("Stream has ended.");
+                      break;
+                  }
+                  
+                  
+                  
+                  }
